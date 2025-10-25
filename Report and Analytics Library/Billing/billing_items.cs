3425eb8b193a2fb0cc7c83bbab4f68be273fd76e
Ezekielskie <deminetel04@gmail.com>
2025-10-26 06:50:12 +0800

    adjusted billing item table

--- Report and Analytics Library/Billing/billing_items.cs
+++ Report and Analytics Library/Billing/billing_items.cs
@@ -23,7 +23,7 @@ namespace Report_and_Analytics_Library.Billing
 
         public int patient_id { get; set; }
         public int service_id { get; set; }
-        //public string item_description { get; set; }
+
         public int quantity { get; set; }
         public decimal? unit_price { get; set; }
         public decimal? total_price { get; set; }
